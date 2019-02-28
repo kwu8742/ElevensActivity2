08f28b6e5c87053fe7e3fb37258b4b463e6d34e9
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2019-02-28 08:41:30 -0500

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,44 @@
-/**
- * Created by Teacher on 1/7/2019.
- */
+package Activity2
+
 public class Card {
+    private String suit;
+    private String rank;
+    private int pointValue;
+
+    public Card(String cardRank, String cardSuit, int cardPointValue) {
+	    this.rank = cardRank;
+	    this.suit = cardSuit;
+	    this.pointValue = cardPointValue;
+    }
+
+    public String suit() {
+	    return suit;
+    }
+
+    public String rank() {
+	    return rank;
+    }
+
+    public int pointValue() {
+	    return pointValue;
+    }
+
+    public boolean matches(Card otherCard) {
+        if (rank.equals(otherCard.rank()))
+        {
+            if (suit.equals(otherCard.suit()))
+            {
+                if (pointValue == otherCard.pointValue()){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString()
+    {
+        return rank + " of " + suit + " (point value = [" + pointValue + "])";
+    }
 }
