ca795f7879e8304322c27b3e9920c52904c12100
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2019-02-28 08:44:25 -0500

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,71 +1,32 @@
-/**
- * Created by Teacher on 1/7/2019.
- */
 import java.util.List;
 import java.util.ArrayList;
 
-/**
- * The Deck class represents a shuffled deck of cards.
- * It provides several operations including
- *      initialize, shuffle, deal, and check if empty.
- */
 public class Deck {
-
-    /**
-     * cards contains all the cards in the deck.
-     */
     private List<Card> cards;
-
-    /**
-     * size is the number of not-yet-dealt cards.
-     * Cards are dealt from the top (highest index) down.
-     * The next card to be dealt is at size - 1.
-     */
     private int size;
 
-
-    /**
-     * Creates a new <code>Deck</code> instance.<BR>
-     * It pairs each element of ranks with each element of suits,
-     * and produces one of the corresponding card.
-     * @param ranks is an array containing all of the card ranks.
-     * @param suits is an array containing all of the card suits.
-     * @param values is an array containing all of the card point values.
-     */
     public Deck(String[] ranks, String[] suits, int[] values) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	    this.cards = new ArrayList<>();
+		for (int i = 0; i < suits.length; i ++) {
+		    for (int j = 0; j < ranks.length; j ++) {
+		        this.cards.add(new Card(ranks[j],suits[i], values[j]));
+            }
+        }
+        this.size = this.cards.size();
     }
 
-
-    /**
-     * Determines if this deck is empty (no undealt cards).
-     * @return true if this deck is empty, false otherwise.
-     */
     public boolean isEmpty() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+	    return this.size <= 0;
     }
 
-    /**
-     * Accesses the number of undealt cards in this deck.
-     * @return the number of undealt cards in this deck.
-     */
     public int size() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		return this.size;
     }
 
-    /**
-     * Randomly permute the given collection of cards
-     * and reset the size to represent the entire deck.
-     */
     public void shuffle() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 4 *** */
     }
 
-    /**
-     * Deals a card from this deck.
-     * @return the card just dealt, or null if all the cards have been
-     *         previously dealt.
-     */
     public Card deal() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
     }
